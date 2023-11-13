0fde5e3c1a54d49e79127dd1c5e69f5763a849fe
mr-jones123 <lacapxyniljhed@gmail.com>
2023-11-13 22:43:53 +0800

    changes made to score and player

--- player.java
+++ player.java
@@ -1,4 +1,26 @@
-
-public class player {
+import java.util.HashMap; //can be used for storing players
+import java.util.Scanner;
+public class player extends score {
+    private String userName;
+    private int playerScore; 
+    Scanner scan = new Scanner(System.in);
+    player(String userName, int playerScore){
+        super(playerScore);
+        this.userName = userName;
+    }
+    // NEW PLAYER 
+    public void newPlayer(){
+        String name;
+        int score;
+        System.out.print("Enter Player Name: ");
+        name = scan.nextLine();
+        System.out.print("Enter Score: ");
+        score = scan.nextInt();
+        player Player = new player(name,score);
+    }
+    //CHECKS IF THE PLAYER ALREADY EXIST, NEEDS CONCRETE IMPLEMENTATION
+    public boolean isExist (){
+        return true;
+    }
     
 }
--- score.java
+++ score.java
@@ -1,3 +1,6 @@
 public class score {
-    
+    protected int highScore;
+    public score (int highScore){
+        this.highScore = highScore;
+    }
 }
